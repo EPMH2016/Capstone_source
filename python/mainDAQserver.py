9a018109aa846b27159296f030658efd70b6b362
EPMH2016 <paul.olivar16@gmail.com>
2015-11-05 10:52:49 -0800

    Added database connectivity to main server script

--- python/mainDAQserver.py
+++ python/mainDAQserver.py
@@ -1,6 +1,9 @@
 import json
 import urllib2
 import time
+import rethinkdb as r
+from time import strftime
+
 
 
 # 7 pages:
@@ -12,13 +15,26 @@ import time
 # 6. Light
 # 7. MACaddress
 
+r.connect('localhost', port=28015, db='Sensor_data').repl()
 
+
+r.table('Sensor1TempHumidity').insert({'message':'This has been inserted from the main DAQ server'}).run()
 
 while True:
    
-    #get temperature in F
-    print("Temperature(F) is " + str(json.load(urllib2.urlopen("http://192.168.1.18/temp"))))
+    time.sleep(70)
 
-    print ("Temperature(C) is " + str(json.load(urllib2.urlopen("http://192.168.1.18/temp_c"))))
     
     
+    #get temperature in F
+    print("Thermocouple 1 is " + str(json.load(urllib2.urlopen("http://10.17.160.120/T1"))))
+
+    print ("Thermocouple 2  is " + str(json.load(urllib2.urlopen("http://10.17.160.120/T2"))))
+    
+    print ('Thermocouple 3 is ' +  str(json.load(urllib2.urlopen("http://10.17.160.120/T3"))))
+    
+    print ('Thermocouple 4 is ' +  str(json.load(urllib2.urlopen("http://10.17.160.120/T4"))))
+
+    print ('Ambient Temperature is ' +  str(json.load(urllib2.urlopen("http://10.17.160.120/AmbientTemp"))))
+
+    print ('Light is ' +  str(json.load(urllib2.urlopen("http://10.17.160.120/Light")))) 
