af02986ebb3df6444d80a6484a6a29466458de45
olivar16 <olivar16@up.edu>
2015-11-19 22:39:32 -0800

    Fixed time error

--- python/mainDAQserver.py
+++ python/mainDAQserver.py
@@ -18,7 +18,7 @@ from time import strftime
 r.connect('localhost', port=28015, db='HDMI').repl()
 
 
-r.table('DAQ1').insert({'message':'This has been inserted from the main DAQ server'}).run()
+#r.table('DAQ1').insert({'message':'This has been inserted from the main DAQ server'}).run()
 
 while True:
    
@@ -33,33 +33,33 @@ while True:
 
     print ("Thermocouple 2  is " + str(json.load(urllib2.urlopen("http://10.17.160.120/T2"))))
     
-    r.table('DAQ1').insert({'Sensor Type': 'T1', 'DAQ ID':'DAQ1f', 'Sensor ID': 'D1T1', 'Data Value': T1, 'Units': 'C', 'Timestamp': r.now().inTimezone('-09:00')})
+    r.table('DAQ1').insert({'Sensor Type': 'T1', 'DAQ ID':'DAQ1f', 'Sensor ID': 'D1T1', 'Data Value': T1, 'Units': 'C', 'Timestamp': r.now().in_timezone('-09:00')}).run()
 
     T2 = float(json.load(urllib2.urlopen("http://10.17.160.120/T2")))
 
-    r.table('DAQ1').insert({'Sensor Type': 'T2', 'DAQ ID':'DAQ1f', 'Sensor ID': 'D1T2', 'Data Value': T2, 'Units': 'C', 'Timestamp': r.now().inTimezone('-09:00')})
+    r.table('DAQ1').insert({'Sensor Type': 'T2', 'DAQ ID':'DAQ1f', 'Sensor ID': 'D1T2', 'Data Value': T2, 'Units': 'C', 'Timestamp': r.now().in_timezone('-09:00')}).run()
 
     print ('Thermocouple 3 is ' +  str(json.load(urllib2.urlopen("http://10.17.160.120/T3"))))
 
     T3 = float(json.load(urllib2.urlopen("http://10.17.160.120/T3")))
     
-    r.table('DAQ1').insert({'Sensor Type': 'T3', 'DAQ ID':'DAQ1f', 'Sensor ID': 'D1T3', 'Data Value': T3, 'Units': 'C', 'Timestamp': r.now().inTimezone('-09:00')})
+    r.table('DAQ1').insert({'Sensor Type': 'T3', 'DAQ ID':'DAQ1f', 'Sensor ID': 'D1T3', 'Data Value': T3, 'Units': 'C', 'Timestamp': r.now().in_timezone('-09:00')}).run()
 
     print ('Thermocouple 4 is ' +  str(json.load(urllib2.urlopen("http://10.17.160.120/T4"))))
 
     T4 = float(json.load(urllib2.urlopen("http://10.17.160.120/T4")))
 
-    r.table('DAQ1').insert({'Sensor Type': 'T4', 'DAQ ID':'DAQ1f', 'Sensor ID': 'D1T4', 'Data Value': T4, 'Units': 'C', 'Timestamp': r.now().inTimezone('-09:00')})
+    r.table('DAQ1').insert({'Sensor Type': 'T4', 'DAQ ID':'DAQ1f', 'Sensor ID': 'D1T4', 'Data Value': T4, 'Units': 'C', 'Timestamp': r.now().in_timezone('-09:00')}).run()
 
     print ('Ambient Temperature is ' +  str(json.load(urllib2.urlopen("http://10.17.160.120/AmbientTemp"))))
 
     AmbientTemp = float(json.load(urllib2.urlopen("http://10.17.160.120/AmbientTemp")))
 
-    r.table('DAQ1').insert({'Sensor Type': 'AmbientTemp', 'DAQ ID':'DAQ1f', 'Sensor ID': 'D1AT', 'Data Value': AmbientTemp, 'Units': 'C', 'Timestamp': r.now().inTimezone('-09:00')})
+    r.table('DAQ1').insert({'Sensor Type': 'AmbientTemp', 'DAQ ID':'DAQ1f', 'Sensor ID': 'D1AT', 'Data Value': AmbientTemp, 'Units': 'C', 'Timestamp': r.now().in_timezone('-09:00')}).run()
 
     print ('Light is ' +  str(json.load(urllib2.urlopen("http://10.17.160.120/Light")))) 
 
     Light = float(json.load(urllib2.urlopen("http://10.17.160.120/Light")))
 
-    r.table('DAQ1').insert({'Sensor Type': 'Light', 'DAQ ID':'DAQ1f', 'Sensor ID': 'D1L', 'Data Value': Light, 'Units': 'lux', 'Timestamp': r.now().inTimezone('-09:00')})
+    r.table('DAQ1').insert({'Sensor Type': 'Light', 'DAQ ID':'DAQ1f', 'Sensor ID': 'D1L', 'Data Value': Light, 'Units': 'lux', 'Timestamp': r.now().in_timezone('-09:00')}).run()
 
