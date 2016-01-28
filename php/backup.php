f9036dc1cfded90ccda8651b55903ec13d2dd135
olivar16 <olivar16@up.edu>
2016-01-27 21:37:57 -0800

    Working email attachments

--- php/backup.php
+++ php/backup.php
@@ -4,5 +4,5 @@ echo "hello world!";
 
 exec("rm -r backup.tar.tgz");
 exec ("rethinkdb dump -f backup.tar.tgz");
-
+exec("../python/backupNotification.py");
 ?>
--- python/backupNotification.py
+++ python/backupNotification.py
@@ -4,17 +4,17 @@ from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
 from smtplib import SMTP
 
-#msg = MIMEText('Hello Mr Reed, \n attached is your backup')
+msg = MIMEText('Hello Mr Reed, \n attached is your backup')
 msg = MIMEMultipart()
 msg['Subject'] = "HDMI Data backup"
-msg['From']    = "Yo Momma"
-msg['To']      = "olivar16@up.edu"
+msg['From']    = "University of Portland"
+msg['To']      = "paul_ryanvper@yahoo.com"
 msg.preamble = "Your data backup"
 
 part = MIMEText("Attached is your data backup")
 msg.attach(part)
 
-part = MIMEApplication(open("../php/backup.tar.tgz", "rb").read())
+part = MIMEApplication(open("backup.tar.tgz", "rb").read())
 part.add_header('Content-Disposition','attachment',filename="backup.tar.tgz")
 msg.attach(part)
 
@@ -24,4 +24,5 @@ s = smtplib.SMTP('smtp.mailgun.org', 587)
 s.login('postmaster@sandbox0343569143484bd0ac8ec34da054003a.mailgun.org', 'epmh2016')
 
 s.sendmail(msg['From'], msg['To'], msg.as_string())
+
 s.quit()
