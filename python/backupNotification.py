3253da381da502ee14b0dd5542ded381a4cf3738
olivar16 <olivar16@up.edu>
2016-01-27 20:59:03 -0800

    Email attachments

--- python/backupNotification.py
+++ python/backupNotification.py
@@ -1,11 +1,22 @@
 import smtplib
-
 from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from smtplib import SMTP
 
-msg = MIMEText('Hello Mr Reed, \n attached is your backup')
+#msg = MIMEText('Hello Mr Reed, \n attached is your backup')
+msg = MIMEMultipart()
 msg['Subject'] = "HDMI Data backup"
 msg['From']    = "Yo Momma"
 msg['To']      = "olivar16@up.edu"
+msg.preamble = "Your data backup"
+
+part = MIMEText("Attached is your data backup")
+msg.attach(part)
+
+part = MIMEApplication(open("../php/backup.tar.tgz", "rb").read())
+part.add_header('Content-Disposition','attachment',filename="backup.tar.tgz")
+msg.attach(part)
 
 #smtp.mailgun.org                                                                                                                                                              
 s = smtplib.SMTP('smtp.mailgun.org', 587)
