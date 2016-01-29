3d41d9a4dce7e812c80f558700d17d426ef18d69
olivar16 <olivar16@up.edu>
2016-01-29 14:44:16 -0800

    modified backup.php python execution context

--- php/backup.php
+++ php/backup.php
@@ -4,5 +4,6 @@ echo "hello world!";
 
 exec("rm -r backup.tar.tgz");
 exec ("rethinkdb dump -f backup.tar.tgz");
-exec("python ../python/backupNotification.py");
+exec("python python/backupNotification.py");
+
 ?>
