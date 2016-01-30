9b900ecd9d72d13de4b1ec388bbb161c3a552275
olivar16 <olivar16@up.edu>
2016-01-29 16:49:01 -0800

    Created PHP backup function

--- php/backup.php
+++ php/backup.php
@@ -5,7 +5,7 @@ echo "hello world!";
 function archivedata(){
 exec("rm -r backup.tar.tgz");
 exec ("rethinkdb dump -f backup.tar.tgz");
-exec("python python/backupNotification.py");
+exec("python ../python/backupNotification.py");
 return 1;
 }
 
