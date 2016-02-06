86c77333f7ff7d14d2805c3fa5f0413e961a9412
olivar16 <olivar16@up.edu>
2016-02-05 16:19:10 -0800

    Purge testing

--- server.js
+++ server.js
@@ -59,11 +59,10 @@ app.get("/purge", function(request, response){
 //     if(err){console.log("Cannot be found")}
 //   });
 // }
-  
-  r.db('Sensor_data').table("Sensor1TempHumidity").delete().run(connection, function(err){
-    if(err){console.log("Cannot be found")}
-  });
   response.send("purge!");
+  // r.db('Sensor_data').table("Sensor1TempHumidity").delete().run(connection, function(err){
+  //   if(err){console.log("Cannot be found")}
+  // });
 
 });
 
