a4ef11352c32b3e8630afef7a886bd852b7fc662
olivar16 <olivar16@up.edu>
2016-02-11 20:51:47 -0800

    Data Acquisition error checking

--- python/mainDAQserver.py
+++ python/mainDAQserver.py
@@ -43,23 +43,25 @@ def DAQ1():
         #Get time interval
         time.sleep(float(timeIntervals['DAQ1']))
         print ("DAQ1:")
-        T1 = float(json.load(urllib2.urlopen("http://10.17.160.120/T1")))
+        #T1 = float(json.load(urllib2.urlopen("http://10.17.160.120/T1")))
+        T1 = sensorCollect("http://10.17.160.120", "DAQ1", "T1")
         print "collected T1"
         print ("T1: " + str(T1))
         r.table('DAQ1').insert({'Sensor Type': 'T1', 'DAQ ID':'DAQ1f', 'Sensor ID': 'D1T1', 'Data Value': T1, 'Units': 'C', 'Timestamp': r.now().in_timezone('-08:00')}).run()
-        T2 = float(json.load(urllib2.urlopen("http://10.17.160.120/T2")))
+        #T2 = float(json.load(urllib2.urlopen("http://10.17.160.120/T2")))
+        T2 = sensorCollect("http://10.17.160.120", "DAQ1", "T2")
         print "T2: " + str(T2)
         r.table('DAQ1').insert({'Sensor Type': 'T2', 'DAQ ID':'DAQ1f', 'Sensor ID': 'D1T2', 'Data Value': T2, 'Units': 'C', 'Timestamp': r.now().in_timezone('-08:00')}).run()
-        T3 = float(json.load(urllib2.urlopen("http://10.17.160.120/T3")))
+        #T3 = float(json.load(urllib2.urlopen("http://10.17.160.120/T3")))
+        T3 = sensorCollect("http://10.17.160.120", "DAQ1", "T3")
         print "T3: " + str(T3)
         r.table('DAQ1').insert({'Sensor Type': 'T3', 'DAQ ID':'DAQ1f', 'Sensor ID': 'D1T3', 'Data Value': T3, 'Units': 'C', 'Timestamp': r.now().in_timezone('-08:00')}).run()
-      #  T4 = float(json.load(urllib2.urlopen("http://10.17.160.120/T4")))
-      #  print "T4: " + str(T4)
-       # r.table('DAQ1').insert({'Sensor Type': 'T4', 'DAQ ID':'DAQ1f', 'Sensor ID': 'D1T4', 'Data Value': T4, 'Units': 'C', 'Timestamp': r.now().in_timezone('-08:00')}).run()
-        AmbientTemp = float(json.load(urllib2.urlopen("http://10.17.160.120/AmbientTemp")))
+        #AmbientTemp = float(json.load(urllib2.urlopen("http://10.17.160.120/AmbientTemp")))
+        AmbientTemp = sensorCollect("http://10.17.160.120", "DAQ1", "AmbientTemp")
         print "AmbientTemp: " + str(AmbientTemp)
         r.table('DAQ1').insert({'Sensor Type': 'AmbientTemp', 'DAQ ID':'DAQ1f', 'Sensor ID': 'D1AT', 'Data Value': AmbientTemp, 'Units': 'C', 'Timestamp': r.now().in_timezone('-08:00')}).run()
-        Light = float(json.load(urllib2.urlopen("http://10.17.160.120/Light")))
+        #Light = float(json.load(urllib2.urlopen("http://10.17.160.120/Light")))
+        Light = sensorCollect("http://10.17.160.120", "DAQ1", "Light")
         r.table('DAQ1').insert({'Sensor Type': 'Light', 'DAQ ID':'DAQ1f', 'Sensor ID': 'D1L', 'Data Value': Light, 'Units': 'lux', 'Timestamp': r.now().in_timezone('-08:00')}).run()
         print "Light:" + str(Light)
 
@@ -99,4 +101,13 @@ if __name__ == "__main__":
 	Thread(target=DAQ2).start()
 	Thread(target=DAQ3).start()
 
+def sensorCollect(url, DAQ, sensorType):
+    finalData = 0.0
+    try:
+        finalData = float(json.load(urllib2.urlopen(url+"/"+sensorType)))
+    except urllib2.URLError:
+        print "Error collecting: " + DAQ + " " + sensorType
+        finalData = float(json.load(urllib2.urlopen(url+"/"+sensorType)))
+    return finalData
+    
 
