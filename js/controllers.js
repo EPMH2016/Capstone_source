632e20b2c5c2c7ca1fa1bbcf063cb6598b612aa5
Michael Thorsen <pnwthorsen@Michaels-MacBook-Pro-2.local>
2016-02-12 14:15:38 -0800

    fixed double load of data graphs

--- js/controllers.js
+++ js/controllers.js
@@ -26,151 +26,8 @@ app.controller("DAQGraphController", function($scope, $http, $q){
 
     $scope.selectedType = "Thermocouple 1 (F)";
 
-    $scope.changeGraphType = function(typeSelected){
-        $scope.selectedType = typeSelected;  
-        console.log("you selected " + $scope.selectedType)     
-       
-       switch ($scope.selectedType){
-        case "T1":
-
-        $http.get('http://10.17.191.41:8435/DAQ1/T1')
-
-        .success(function(data_daq1, status, headers, config){
-            $scope.DAQ1($scope.selectedType, data_daq1, data_daq2, data_daq3, data_daq4);
-        })
-        .error(function(data_daq1, status, headers, config){
-            console.log("Error, data not found.");
-        });
-
-        $http.get('http://10.17.191.41:8435/DAQ2/T1')
-
-        .success(function(data_daq2, status, headers, config){
-            $scope.DAQ1($scope.selectedType, data_daq1, data_daq2, data_daq3, data_daq4);
-        })
-        .error(function(data_daq2, status, headers, config){
-            console.log("Error, data not found.");
-        });
-
-        break;
-       case "T2":
-       
-        $http.get('http://10.17.191.41:8435/DAQ1/T2')
-
-        .success(function(data_daq1, status, headers, config){
-            $scope.DAQ1($scope.selectedType, data_daq1, data_daq2, data_daq3, data_daq4);
-        })
-        .error(function(data_daq1, status, headers, config){
-            console.log("Error, data not found.");
-        });
-
-        $http.get('http://10.17.191.41:8435/DAQ2/T2')
-
-        .success(function(data_daq2, status, headers, config){
-            $scope.DAQ1($scope.selectedType, data_daq1, data_daq2, data_daq3, data_daq4);
-        })
-        .error(function(data_daq2, status, headers, config){
-            console.log("Error, data not found.");
-        });
-
-       break;
-       case "T3":
-        
-        $http.get('http://10.17.191.41:8435/DAQ1/T3')
-
-        .success(function(data_daq1, status, headers, config){
-            $scope.DAQ1($scope.selectedType, data_daq1, data_daq2, data_daq3, data_daq4);
-        })
-        .error(function(data_daq1, status, headers, config){
-            console.log("Error, data not found.");
-        });
-
-        $http.get('http://10.17.191.41:8435/DAQ2/T3')
-
-        .success(function(data_daq2, status, headers, config){
-            $scope.DAQ1($scope.selectedType, data_daq1, data_daq2, data_daq3, data_daq4);
-        })
-        .error(function(data_daq2, status, headers, config){
-            console.log("Error, data not found.");
-        });
-
-       break;
-       case "T4":
-        
-        $http.get('http://10.17.191.41:8435/DAQ1/T4')
-
-        .success(function(data_daq1, status, headers, config){
-            $scope.DAQ1($scope.selectedType, data_daq1, data_daq2, data_daq3, data_daq4);
-        })
-        .error(function(data_daq1, status, headers, config){
-            console.log("Error, data not found.");
-        });
-
-        $http.get('http://10.17.191.41:8435/DAQ2/T4')
-
-        .success(function(data_daq2, status, headers, config){
-            $scope.DAQ1($scope.selectedType, data_daq1, data_daq2, data_daq3, data_daq4);
-        })
-        .error(function(data_daq2, status, headers, config){
-            console.log("Error, data not found.");
-        });
-
-       break;
-       case "Light":
-
-        $http.get('http://10.17.191.41:8435/DAQ1/Light')
-
-        .success(function(data_daq1, status, headers, config){
-            $scope.DAQ1($scope.selectedType, data_daq1, data_daq2, data_daq3, data_daq4);
-        })
-        .error(function(data_daq1, status, headers, config){
-            console.log("Error, data not found.");
-        });
-
-        $http.get('http://10.17.191.41:8435/DAQ2/Light')
-
-        .success(function(data_daq2, status, headers, config){
-            $scope.DAQ1($scope.selectedType, data_daq1, data_daq2, data_daq3, data_daq4);
-        })
-        .error(function(data_daq2, status, headers, config){
-            console.log("Error, data not found.");
-        });
-
-       break;
-       case "Ambient":
-                      
-        $http.get('http://10.17.191.41:8435/DAQ1/AmbientTemp')
-
-        .success(function(data_daq1, status, headers, config){
-            $scope.DAQ1($scope.selectedType, data_daq1, data_daq2, data_daq3, data_daq4);
-        })
-        .error(function(data_daq1, status, headers, config){
-            console.log("Error, data not found.");
-        });
-
-        $http.get('http://10.17.191.41:8435/DAQ2/AmbientTemp')
-
-        .success(function(data_daq2, status, headers, config){
-            $scope.DAQ1($scope.selectedType, data_daq1, data_daq2, data_daq3, data_daq4);
-        })
-        .error(function(data_daq2, status, headers, config){
-            console.log("Error, data not found.");
-        });
-
-       break;
-       }
-        
-    }
-    
-
-    $scope.changeGraphType("T1");
-    
-
-    $scope.hello = function(){
-        console.log("Hello, world!");
-    }
-    
-    
-    $scope.DAQ1 = function(sensorType, data_daq1, data_daq2, data_daq3, data_daq4){
+    //  make this a different function that simply collects and allocates the data  
+    $scope.collect_data = function(sensorType, data_daq1, data_daq2, data_daq3, data_daq4, print_graph){
         if(data_daq1.length != 0)
         {
             data_array_daq1 = [];
@@ -185,8 +42,8 @@ app.controller("DAQGraphController", function($scope, $http, $q){
 
         if(data_daq3.length != 0)
         {
-            data_array_3 = [];
-            date_array = [];
+            data_array_daq3 = [];
+            date_array_daq3 = [];
         }
 
         if(data_daq4.length != 0)
@@ -195,11 +52,17 @@ app.controller("DAQGraphController", function($scope, $http, $q){
             date_array_daq4 = [];
         }
 
+        // console.log(data_array_daq1)
+
 
        for(i = 0; i < data_daq1.length; i++)
         {
                 data_array_daq1.unshift(data_daq1[i]["Data Value"]);
-                date_array_daq1.unshift(data_daq1[i]["Timestamp"])
+                var date = new Date(data_daq1[i]["Timestamp"]);
+                date_array_daq1.unshift(data_daq1[i]["Timestamp"]);
+                //console.log(date.getTime());
+                //console.log(date);
+                console.log(Date.parse(date_array_daq1[i]));
         }
 
         for(i = 0; i < data_daq2.length; i++)
@@ -219,10 +82,17 @@ app.controller("DAQGraphController", function($scope, $http, $q){
                 data_array_daq4.push(data_daq4[i]["Data Value"]);
                 date_array_daq4.push(data_daq4[i]["Timestamp"])
         }
+
+        if(print_graph)
+        {
+            $scope.print_graph();
+        }
+    }
             
 
 
-
+// make a function here with the only purpose to be printing the graph
+    $scope.print_graph = function(){
 
         var graph_array_daq1 = [];
 
@@ -245,13 +115,17 @@ app.controller("DAQGraphController", function($scope, $http, $q){
 
 
 
-       // console.log("The selected sensor is " + sensorType)
 
         
         var chart = new Highcharts.Chart({
             credits: {
             enabled: false
         },
+
+        global: {
+                useUTC: false
+        },
+
         chart: {
             renderTo: 'container',
             type: 'line'
@@ -295,7 +169,158 @@ app.controller("DAQGraphController", function($scope, $http, $q){
     });
         
     }
+
+    $scope.changeGraphType = function(typeSelected){
+        $scope.selectedType = typeSelected;
+        console.log("you selected " + $scope.selectedType)     
+
+       switch ($scope.selectedType){
+        case "T1":
+            $http.get('http://10.17.191.41:8435/DAQ1/T1')
+
+            .success(function(data_daq1, status, headers, config){
+                $scope.collect_data($scope.selectedType, data_daq1, data_daq2, data_daq3, data_daq4, false);
+            })
+            .error(function(data_daq1, status, headers, config){
+                console.log("Error, data not found.");
+            });
+
+            $http.get('http://10.17.191.41:8435/DAQ2/T1')
+
+            .success(function(data_daq2, status, headers, config){
+                $scope.collect_data($scope.selectedType, data_daq1, data_daq2, data_daq3, data_daq4, true);
+            })
+            .error(function(data_daq2, status, headers, config){
+                console.log("Error, data not found.");
+            });
+
+
+        break;
+        case "T2":
+            $http.get('http://10.17.191.41:8435/DAQ1/T2')
+
+            .success(function(data_daq1, status, headers, config){
+                $scope.collect_data($scope.selectedType, data_daq1, data_daq2, data_daq3, data_daq4, false);
+            })
+            .error(function(data_daq1, status, headers, config){
+                console.log("Error, data not found.");
+            });
+
+            $http.get('http://10.17.191.41:8435/DAQ2/T2')
+
+            .success(function(data_daq2, status, headers, config){
+                $scope.collect_data($scope.selectedType, data_daq1, data_daq2, data_daq3, data_daq4, true);
+            })
+            .error(function(data_daq2, status, headers, config){
+                console.log("Error, data not found.");
+            });
+
+
+        break;
+        case "T3":
+        
+            $http.get('http://10.17.191.41:8435/DAQ1/T3')
+
+            .success(function(data_daq1, status, headers, config){
+                $scope.collect_data($scope.selectedType, data_daq1, data_daq2, data_daq3, data_daq4, false);
+            })
+            .error(function(data_daq1, status, headers, config){
+                console.log("Error, data not found.");
+            });
+
+            $http.get('http://10.17.191.41:8435/DAQ2/T3')
+
+            .success(function(data_daq2, status, headers, config){
+                $scope.collect_data($scope.selectedType, data_daq1, data_daq2, data_daq3, data_daq4, true);
+            })
+            .error(function(data_daq2, status, headers, config){
+                console.log("Error, data not found.");
+            });
+
+
+        break;
+        case "T4":
+        
+            $http.get('http://10.17.191.41:8435/DAQ1/T4')
+
+            .success(function(data_daq1, status, headers, config){
+                $scope.collect_data($scope.selectedType, data_daq1, data_daq2, data_daq3, data_daq4, false);
+            })
+            .error(function(data_daq1, status, headers, config){
+                console.log("Error, data not found.");
+            });
+
+            $http.get('http://10.17.191.41:8435/DAQ2/T4')
+
+            .success(function(data_daq2, status, headers, config){
+                $scope.collect_data($scope.selectedType, data_daq1, data_daq2, data_daq3, data_daq4, true);
+            })
+            .error(function(data_daq2, status, headers, config){
+                console.log("Error, data not found.");
+            });
+
+
+        break;
+        case "Light":
+
+            $http.get('http://10.17.191.41:8435/DAQ1/Light')
+
+            .success(function(data_daq1, status, headers, config){
+                $scope.collect_data($scope.selectedType, data_daq1, data_daq2, data_daq3, data_daq4, false);
+            })
+            .error(function(data_daq1, status, headers, config){
+                console.log("Error, data not found.");
+            });
+
+            $http.get('http://10.17.191.41:8435/DAQ2/Light')
+
+            .success(function(data_daq2, status, headers, config){
+                $scope.collect_data($scope.selectedType, data_daq1, data_daq2, data_daq3, data_daq4, true);
+            })
+            .error(function(data_daq2, status, headers, config){
+                console.log("Error, data not found.");
+            });
+
+
+        break;
+        case "Ambient":
+                      
+            $http.get('http://10.17.191.41:8435/DAQ1/AmbientTemp')
+
+            .success(function(data_daq1, status, headers, config){
+                $scope.collect_data($scope.selectedType, data_daq1, data_daq2, data_daq3, data_daq4, false);
+            })
+            .error(function(data_daq1, status, headers, config){
+                console.log("Error, data not found.");
+            });
+
+            $http.get('http://10.17.191.41:8435/DAQ2/AmbientTemp')
+
+            .success(function(data_daq2, status, headers, config){
+                $scope.collect_data($scope.selectedType, data_daq1, data_daq2, data_daq3, data_daq4, true);
+            })
+            .error(function(data_daq2, status, headers, config){
+                console.log("Error, data not found.");
+            });
+
+
+        break;
+        default:
+
+            console.log("Selected Type is not a valid type: " + typeSelected);
+
+        break;
+       }
+        
+    }
     
+
+    $scope.changeGraphType("T1");
+    
+
+    $scope.hello = function(){
+        console.log("Hello, world!");
+    }
     
 
 });
