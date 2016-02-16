4dc8105e0a091be4828e0b62a41252e9e43897a0
olivar16 <olivar16@up.edu>
2016-02-15 19:15:12 -0800

    Post requests for DAQ config

--- server.js
+++ server.js
@@ -38,10 +38,12 @@ console.log("Successfully connected to database");
 
 app.use("/", express.static(__dirname));
 
+app.use(express.urlencoded());
+app.use(express.json());  
 
 app.use("/postSample", function(request, response){
 console.log("The request is " + request);
-console.log("The name is " + request.body);
+console.log("The name is " + request.body.name);
 });
 
 //Archive - Email the data to the client in JSON format
