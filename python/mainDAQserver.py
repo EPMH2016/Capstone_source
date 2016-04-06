a1356747e9311edbba85fb78dc1e23ea66a4174e
olivar16 <olivar16@up.edu>
2016-04-05 21:54:05 -0700

    a

--- python/mainDAQserver.py
+++ python/mainDAQserver.py
@@ -109,7 +109,7 @@ def sensorCollect(url, DAQ, sensorType, DAQid, sensorId,  units):
     finalData = "empty"
     try:
        # finalData = float(json.load(mechanize.urlopen(url+"/"+sensorType)))
-       finalData = requests.get(url+"/"+sensorType, timeout=10).json()
+       finalData = requests.get(url+"/"+sensorType, timeout=5).json()
        print DAQ + ": Data collection success"
        #urllib2.URLError,BadStatusLine
     except (ValueError):
